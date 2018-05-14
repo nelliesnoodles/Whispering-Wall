8489896e8876cc6cc6bb9706c8187c9752d537f3
Nellie Tobey <35578867+nelliesnoodles@users.noreply.github.com>
2018-05-14 12:20:28 -0700

    Add files via upload
    newer Wiwa with question responses

--- wiwa_class.py
+++ wiwa_class.py
@@ -9,12 +9,14 @@ import nltk as nltk
 
 
 
+
 """
   Requires:
-  	above imports and :
+    *Running setup1.py in linux (&&) and in your python3 virtual env will set all this up for you*
+  	Manual setup:
   	install - nltk
   	install - python3
-  	install - PyEnchant  
+  	install - PyEnchant
   	In your python3 shell type these to download needed data sets:
   	>>>import nltk
   	>>>nltk.download('wordnet')
@@ -23,6 +25,7 @@ import nltk as nltk
   	noun responses -- requires script1 as sys.argv
   	verb responses -- requires script2 as sys.argv
   	yes responses -- requires script3
+    questionable -- script4
 """
 
 
@@ -34,12 +37,13 @@ class Wiwa(object):
 		self.nounscript = sys.argv[1]
 		self.verbscript = sys.argv[2]
 		self.simplescript = sys.argv[3]
+		self.questionable = sys.argv[4]
 		self.dictionary = enchant.Dict("en_US")
-	
+
 	def run_wiwa(self):
 		intro = """ Welcome to the whispering wall, Wiwa is here to respond
 		and question your perspective. She is just a program, and
-		will respond according to her script.  
+		will respond according to her script.
 		If you wish to stop program, type EXIT or QUIT.
 		Have fun! """
 		print(intro)
@@ -53,12 +57,11 @@ class Wiwa(object):
 			av = len(adv)
 			er = len(errors)
 			question = self.check_question(make)
-			if make == "":
-				print("Need INPUT!")
-			elif question:
+			if question:
 				# Maybe use simple script for these too?
 				print("Wiwa:")
-				print("I have not formulated how to respond to questions yet.")
+				discusanswer = self.get_script_line(self.questionable)
+				print(discusanswer)
 			elif make in ['yes', 'no', 'maybe']:
 				response = self.get_script_line(self.simplescript)
 				print("Wiwa:")
@@ -81,8 +84,8 @@ class Wiwa(object):
 				print(f"I wish I could do it {adv[0]}")
 			else:
 				print("Wiwa: I do not comprehend")
-			make = input("...>>") 	
-	
+			make = input("...>>")
+
 	def get_script_line(self, arg):
 		with open(arg) as f:
 			for i, l in enumerate(f):
@@ -93,22 +96,22 @@ class Wiwa(object):
 				lines = f.readlines()
 				x = randint(0, count)
 				return lines[x]
-				
+
 	def strip_stop_words(self, arg):
-		stops = ['the', 'of', 'he', 'she', 'it', 'some', 'all', 'a', 'lot', 
+		stops = ['the', 'of', 'he', 'she', 'it', 'some', 'all', 'a', 'lot',
 				'have', 'about', 'been', 'to', 'too', 'from', 'an', 'at',
 				'above', 'before', 'across', 'against', 'almost', 'along', 'aslo',
 				'although', 'always', 'am', 'among', 'amongst', 'amount', 'and',
-				'another', 'any', 'anyhow', 'anyone', 'anything', 'around', 'as', 
-				'be', 'maybe', 'being', 'beside', 'besides', 'between', 'beyond', 'both', 
-				'but', 'by', 'can', 'could', 'done', 'during', 'each', 'either', 
-				'else', 'even', 'every', 'everyone', 'everything', 'everywhere', 
-				'except', 'few', 'for', 'had', 'has', 'hence', 'here', 'in', 'into', 'is', 
+				'another', 'any', 'anyhow', 'anyone', 'anything', 'around', 'as',
+				'be', 'maybe', 'being', 'beside', 'besides', 'between', 'beyond', 'both',
+				'but', 'by', 'can', 'could', 'done', 'during', 'each', 'either',
+				'else', 'even', 'every', 'everyone', 'everything', 'everywhere',
+				'except', 'few', 'for', 'had', 'has', 'hence', 'here', 'in', 'into', 'is',
 				'it', 'its', 'keep', 'last', 'latter', 'many', 'may', 'more', 'most',
-				'much', 'name', 'next', 'none', 'not', 'nothing', 'now', 'nowhere', 
-				'often', 'other', 'others', 'over', 'rather', 'perhaps', 'seems', 'then', 
-				'there', 'these', 'they', 'though', 'thru', 'too', 'under', 'until', 
-				'upon', 'very', 'was', 'were' 'which', 'while', 'will', 'with', 'ill', 'lets'] 
+				'much', 'name', 'next', 'none', 'not', 'nothing', 'now', 'nowhere',
+				'often', 'other', 'others', 'over', 'rather', 'perhaps', 'seems', 'then',
+				'there', 'these', 'they', 'though', 'thru', 'too', 'under', 'until',
+				'upon', 'very', 'was', 'were' 'which', 'while', 'will', 'with', 'ill', 'lets']
 		new_arg = []
 		for item in arg:
 			if item in stops:
@@ -116,11 +119,11 @@ class Wiwa(object):
 			else:
 				new_arg.append(item)
 		#print(new_arg)
-		return new_arg			
-		
+		return new_arg
+
 	def make_tag_lists(self, arg):
 	#With string as arg, return lists of Adj, adv, noun, verb
-	#    Empty lists mean no found type-noun(verb, adv, adj)	
+	#    Empty lists mean no found type-noun(verb, adv, adj)
 		clean_arg, errors = self.clean_string(arg)
 		tokens = nltk.word_tokenize(clean_arg)
 		tags = nltk.pos_tag(tokens)
@@ -146,9 +149,9 @@ class Wiwa(object):
 		adj = self.strip_stop_words(adj)
 		adv = self.strip_stop_words(adv)
 		return nouns, verbs, adj, adv, errors
-		
+
 	def clean_string(self, astring):
-		""" Take the string and clear out all irrelevant data. 
+		""" Take the string and clear out all irrelevant data.
 		Goal: to identify main verbs and nouns, any adjectives, adverbs
 		Only tag words that are in the PyEnchant English dictionary. """
 		astring = str(astring)
@@ -162,26 +165,26 @@ class Wiwa(object):
 			if self.enchant_check(word) == True:
 				cleaned = cleaned + word + " "
 			else:
-				#print(f"word not found {word}") 
+				#print(f"word not found {word}")
 				errors.append(word)
-				
+
 		return cleaned, errors
-		
+
 	def enchant_check(self, arg):
 		""" using the PyEnchant English dictionary to check validity of a word."""
 		x = self.dictionary.check(arg)
-		return x 
-	
+		return x
+
 	def check_question(self, arg):
 		questions = ['why', '?']
 		if questions[0] in arg or questions[1] in arg:
 			return True
 		else:
 			return False
-			
-		
-		
-		
-		
+
+
+
+
+
 new_wiwa = Wiwa()
 new_wiwa.run_wiwa()
